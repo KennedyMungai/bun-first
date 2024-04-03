6406d4227e65351c84df9baf669346ca4360f656
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-03 14:34:03 +0300

    Added some logic to the index ts file

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
+import figlet from 'figlet'
+
 const server = Bun.serve({
 	port: 3000,
-	fetch: (request) => {
-		return new Response('Hello, World!', {
-			headers: {
-				'Content-Type': 'text/plain'
-			}
-		})
+	fetch: (req) => {
+		const body = figlet.textSync('Hello World!')
+		return new Response('Hello World')
 	}
 })
 
-console.log('Web Server Active')
+console.log(`Serving on http://localhost:${server.port}`)
