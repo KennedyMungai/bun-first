e972873868ff3241a7cba0632b6a6af1a5deaa25
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-03 14:43:02 +0300

    Added some routes to the bun server

--- index.ts
+++ index.ts
@@ -3,8 +3,18 @@ import figlet from 'figlet'
 const server = Bun.serve({
 	port: 3000,
 	fetch: (req) => {
-		const body = figlet.textSync('Run my race!')
-		return new Response(body)
+		const url = new URL(req.url)
+
+		if (url.pathname === '/') {
+			const body = figlet.textSync('Run my race!')
+			return new Response(body)
+		}
+
+		if (url.pathname === '/about') {
+			return new Response('About Me!')
+		}
+
+		return new Response('404!')
 	}
 })
 
